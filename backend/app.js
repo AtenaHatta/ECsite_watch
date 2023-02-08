c571bc0cdf21cfde97b18969fa83f9dc53c13ca5
Atena Hatta <atn041102@gmail.com>
2023-02-07 22:35:13 -0800

    swdefrgthyujik

--- backend/app.js
+++ backend/app.js
@@ -4,82 +4,81 @@ var app = express()
 
 app.use(cors())
 
-// simulates a database containing 4 watches with name, image, desc
+// simulates a database containing 12 watches with name, image, desc
 
 const watches = [
     {
         "id" : 1,
         "Name" : "Watch 1",
         "Description" : "Watch 1 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
 
     {
         "id" : 2,
         "Name" : "Watch 2",
         "Description" : "Watch 2 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
     {
         "id" : 3,
         "Name" : "Watch 3",
         "Description" : "Watch 3 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmtKFAPHH40dYyMF7PXhi4jf5wi6BzHXB0INdh3PMZ_gkCOnYwSwBorTmwc9VilPHzUzc&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
     {
         "id" : 4,
         "Name" : "Watch 4",
         "Description" : "Watch 4 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTiroHvzawCs1rdG5qG06gQI35__X0xEdHhKQ&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
     {
         "id" : 5,
         "Name" : "Watch 1",
         "Description" : "Watch 1 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
     {
         "id" : 6,
         "Name" : "Rolex",
         "Description" : "3546",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU", 
-                   "https://content.rolex.com/dam/2022-11/upright-bba-with-shadow/m126720vtnr-0001.png?impolicy=v6-upright&imwidth=270" ]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://content.rolex.com/dam/2022-11/upright-bba-with-shadow/m126720vtnr-0001.png?impolicy=v6-upright&imwidth=270" ]
     },
     {
         "id" : 7,
         "Name" : "Watch 1",
         "Description" : "Watch 1 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
     {
         "id" : 8,
         "Name" : "Watch 1",
         "Description" : "Watch 1 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
     {
         "id" : 9,
         "Name" : "Watch 1",
         "Description" : "Watch 1 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
     {
         "id" : 10,
         "Name" : "Watch 1",
         "Description" : "Watch 1 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
     {
         "id" : 11,
         "Name" : "Watch 1",
         "Description" : "Watch 1 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     },
     {
         "id" : 12,
         "Name" : "Watch 1",
         "Description" : "Watch 1 description",
-        "image" : ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_N3O7AO4gnk1lnQZRUS1cfA_HG5Za50KmUMRWN3e4fto51Vd8TDcWNselEiOIpZG09II&usqp=CAU"]
+        "image" : ["https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw192c8222/images/large/637709174551867959-2059087.png?sw=750&sh=750&sm=fit&sfrm=png", "https://www.cartier.com/dw/image/v2/BGTJ_PRD/on/demandware.static/-/Sites-cartier-master/default/dw490a1338/images/large/637709174551867959-2059117.png?sw=2000&sh=2000&sm=fit&sfrm=png"]
     }
 
 
--- index1.js
+++ index1.js
@@ -41,13 +41,11 @@ function smoothScroll(e) {
     window.scrollTo({
         top: scrollPosition,
         behavior: "smooth"
-        
     });
 }
 
 
 // Shopping cart menu ----------------------------------------
-
 document.querySelector('.menu-btn').addEventListener('click', function(){
   document.querySelector('.menu').classList.toggle('is-active');
 });
