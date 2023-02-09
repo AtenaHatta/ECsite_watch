c4ddebef1b10ee444cab496393448d597eaab975
OdinOfRivia <gyunay@hotmail.dk>
2023-02-09 01:41:31 -0800

    345678

--- index2.js
+++ index2.js
@@ -24,7 +24,7 @@ fetch("http://localhost:3000/GetAllWatches")
 
       var image = document.createElement("img");
       image.setAttribute("src", watch["image"][0]);
-      image.setAttribute("class","apiImg")
+      image.setAttribute("class", "apiImg");
 
       nextBtn.addEventListener("click", () => {
         const currentImageSrc = image.src;
@@ -46,12 +46,13 @@ fetch("http://localhost:3000/GetAllWatches")
         } else {
           image.src = watch["image"][watch["image"].length - 1];
         }
+
+        
       });
 
       //H4
       var h4 = document.createElement("h4");
       h4.textContent = watch["Name"];
-
 
       //P tag
       var para = document.createElement("p");
@@ -62,109 +63,119 @@ fetch("http://localhost:3000/GetAllWatches")
       btn.setAttribute("class", "add-btn");
       btn.textContent = "ADD TO BAG";
 
-
-
-      btn.addEventListener('click', () => {
-        //we create the div item 
-        var cartDivItem = document.createElement('div')
-        cartDivItem.setAttribute('class', 'cart-item')
+      btn.addEventListener("click", () => {
+        //we create the div item
+        var cartDivItem = document.createElement("div");
+        cartDivItem.setAttribute("class", "cart-item");
 
         //we create the img tag
-        var cartImage = document.createElement('img')
-        cartImage.src = watch["image"]
-        
+        var cartImage = document.createElement("img");
+        cartImage.src = watch["image"];
+
         //we create the h4 tag
-        var cartH4 = document.createElement('h4')
-        cartH4.textContent = watch["Name"]
+        var cartH4 = document.createElement("h4");
+        cartH4.textContent = watch["Name"];
 
         //we create the h5 tag
-        var cartH5 = document.createElement('h5')
-        cartH5.textContent = watch["Description"]
+        var cartH5 = document.createElement("h5");
+        cartH5.textContent = watch["Description"];
 
-        //we create the span
-        var cartSpan = document.createElement('span')
-        cartSpan.setAttribute('class', 'remove-item')
-        cartSpan.textContent = "remove"
+        //we create the span that removes one item
+        var cartSpan = document.createElement("span");
+        cartSpan.setAttribute("id", "remove-item");
+        cartSpan.textContent = "remove";
 
         //we create the arrow up
-        var arrowUp = document.createElement('i')
-        arrowUp.setAttribute('class', 'fas fa-chevron-up higher-amount')
+        var arrowUp = document.createElement("i");
+        arrowUp.setAttribute("class", "fas fa-chevron-up higher-amount");
 
         //we create the p tag
-        var cartPara = document.createElement('p')
-        cartPara.setAttribute('class', 'item-amount')
-        cartPara.textContent = '1'
+        var cartPara = document.createElement("p");
+        cartPara.setAttribute("class", "item-amount");
+        cartPara.textContent = "1";
 
         // we create the arrow down item
-        var arrowDown = document.createElement('i')
-        arrowDown.setAttribute('class', 'fas fa-chevron-down lower-amount')
+        var arrowDown = document.createElement("i");
+        arrowDown.setAttribute("class", "fas fa-chevron-down lower-amount");
 
-        cartDivItem.append(cartImage)
-        cartDivItem.append(cartH4)
-        cartDivItem.append(cartH5)
-        cartDivItem.append(cartSpan)
-        cartDivItem.append(arrowUp)
-        cartDivItem.append(cartPara)
-        cartDivItem.append(arrowDown)
+        cartDivItem.append(cartImage);
+        cartDivItem.append(cartH4);
+        cartDivItem.append(cartH5);
+        cartDivItem.append(cartSpan);
+        cartDivItem.append(arrowUp);
+        cartDivItem.append(cartPara);
+        cartDivItem.append(arrowDown);
 
-        cartContainer.append(cartDivItem)
-        
-       //document.body.insertBefore (cartContainer, appBef)
+        cartContainer.append(cartDivItem);
 
-
-       //////////////////------------////////////////////
-        //cart top footer section
+       //////////////===================///////////////////
 
         
+        cartValue.textContent = watch["Description"];
 
-
-
-
-        //////////////////------------////////////////////
-        cartValue.textContent = watch["Description"]
-
-
-
-
-        var cartItems = document.querySelector(".cart-item")
-
-        // cartLogic(){
-        //   removeBtn.addEventListener('click', () => {
-        //     cartItems.remove();
-        //   })
-        // }
-
-        // clearCart(){
-        //   let cartItems2 = cartItems.map()
-        // }
         
-
-        // removeBtn.addEventListener('click', () => {
-        //   document.querySelector('.cart-content').innerHTML=`<div class="cart-footer">
-        //   <h3>your total : <span class="cart-total">0</span></h3>
-        //   <button class="clear-cart banner-btn" id="removeAll">clear cart</button>
-        // </div>`
+        //remove all items
+        // var cartItems = document.querySelector(".cart-item");
+        // removeBtn.addEventListener("click", () => {
+        //   cartItems.remove();
+        // });
+        
+        //remove one items 
+        // cartContainer.addEventListener('click', (e) => {
+        //   console.dir(e.target);
+        //   if (e.target.parentElement.childNodes[3] === "remove-item") {
+        //     const listToDelete = e.target.parentNode;
+        //     cartItems.removeChild(listToDelete)            
+        //   }
         // })
         
+        
+        removeAll.addEventListener("click", deleteProduct);
+        function deleteProduct(e) {
+          let cartItem;
+          if (e.target.tagName === "awd") {
+            cartItem = e.target.parentElement;
+            cartItem.remove(); // this removes from the DOM only
+          } else if (e.target.tagName === "BUTTON") {
+            cartItem = e.target.parentElement;
+            cartItem.previousSibling.remove(); // this removes from the DOM only
+          }}
+
+        
+
+
       })
-      
-      
+
       div.append(image);
       div.append(prevBtn);
       div.append(nextBtn);
-      
+
       li.append(div);
       li.append(h4);
       li.append(para);
       li.append(btn);
-      
+
       container.append(li);
     });
-    
-    const removeBtn = document.getElementById("removeAll")
-    const cartValue = document.querySelector('.cart-total')
-    const cartContainer = document.querySelector('.cart-content')
+
+    const removeAll = document.querySelector(".cart-footer");
+    const removeBtn = document.getElementById("removeAll");
+    const cartValue = document.querySelector(".cart-total");
+    const cartContainer = document.querySelector(".cart-content");
     //Using this will select ONLY the first image that has the next-btn class
     // const btnRight = document.querySelector(".next-btn");
     // const btnLeft = document.querySelector(".prev-btn");
   });
+  
+
+
+
+//Header Scroll background change  
+ const headerColor = document.querySelector(".header-fixed");
+ window.onscroll = () => {
+  if (window.scrollY > 100) {
+    headerColor.classList.add("header-active");
+  } else {
+    headerColor.classList.remove("header-active");
+  }
+};
